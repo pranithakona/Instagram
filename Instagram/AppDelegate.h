be258d736fd5c6e8722800a6dbc5ffdf8e1eb520
Pranitha Kona <71575891+pranithakona@users.noreply.github.com>
2021-07-06 14:25:12 -0500

    first commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Pranitha Reddy Kona on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Pranitha Reddy Kona on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
